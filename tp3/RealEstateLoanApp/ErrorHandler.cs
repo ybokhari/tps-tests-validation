656754fb1c2f6f609db72c5e5e9a2b3741642909
ybokhari <younes.bokhari@outlook.com>
2024-03-24 11:59:27 +0100

    ✅ Create ErrorHandler class + tests for IsLoanAmountValid()

---
+++ tp3/RealEstateLoanApp/ErrorHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateLoanApp
+{
+    public class ErrorHandler
+    {
+        public bool IsLoanAmountValid(int loanAmount)
+        {
+            if (loanAmount < 50000)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ tp3/RealEstateLoanTests/ErrorHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using RealEstateLoanApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateLoanTests
+{
+    public class ErrorHandlerTests
+    {
+        [Theory]
+        [InlineData(-70500)]
+        [InlineData(0)]
+        [InlineData(49999)]
+        public void LoanAmountIsLessThanMinTest(int loanAmount)
+        {
+            ErrorHandler errorHandler = new ErrorHandler();
+
+            bool result = errorHandler.IsLoanAmountValid(loanAmount);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(50000)]
+        [InlineData(100000)]
+        [InlineData(1000000)]
+        public void LoanAmountIsGreaterThanMinTest(int loanAmount)
+        {
+            ErrorHandler errorHandler = new ErrorHandler();
+
+            bool result = errorHandler.IsLoanAmountValid(loanAmount);
+
+            Assert.True(result);
+        }
+    }
+}
