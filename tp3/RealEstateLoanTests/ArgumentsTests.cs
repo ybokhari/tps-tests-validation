2132b5aee644fc2f86a48bc4005a9f6bcf1d2d7d
ybokhari <younes.bokhari@outlook.com>
2024-03-26 11:49:54 +0100

    ✅ Tests for ParseTest()

--- tp3/RealEstateLoanTests/ArgumentsTests.cs
+++ tp3/RealEstateLoanTests/ArgumentsTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RealEstateLoanApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +7,65 @@ using System.Threading.Tasks;
 
 namespace RealEstateLoanTests
 {
-    internal class ArgumentsTests
+    public class ArgumentsTests
     {
+        private Arguments arguments;
+
+        public ArgumentsTests()
+        {
+            arguments = new Arguments();
+        }
+
         [Theory]
         [InlineData("50000", "108", "0.012")]
-        [InlineData("100000", "108", "0.015")]
-        [InlineData("200000", "180", "0.032")]
+        [InlineData("200000", "180", "0,032")]
         public void ParseTest(string loanAmount, string duration, string nominalRate)
         {
             string[] args = new string[] { loanAmount, duration, nominalRate };
 
-            var result = Arguments.Parse(args);
+            var result = Record.Exception(() => arguments.Parse(args));
 
-            Assert.AreEqual(100000, result.LoanAmount);
-            Assert.AreEqual(240, result.Duration);
-            Assert.AreEqual(0.03, result.NominalRate);
+            Assert.Null(result);
+        }
+
+        [Theory]
+        [InlineData("", "", "")]
+        [InlineData("50000", "", "")]
+        [InlineData("50000", "108", "")]
+        public void ParseTest_Only3Parameters(string loanAmount, string duration, string nominalRate)
+        {
+            string[] args = new string[] { loanAmount, duration, nominalRate };
+            args = args.Where(arg => !string.IsNullOrEmpty(arg)).ToArray(); // Filter empty strings
+            var exceptionType = typeof(ArgumentException);
+            var expectedMessage = "The calculator accept only 3 parameters";
+
+            var result = Record.Exception(() => arguments.Parse(args));
+
+            Assert.NotNull(result);
+            Assert.IsType(exceptionType, result);
+            Assert.Equal(expectedMessage, result.Message);
+        }
+
+        [Theory]
+        [InlineData("", "108", "0.012")]
+        [InlineData("50000", "", "0.012")]
+        [InlineData("50000", "108", "")]
+        [InlineData("50000", "108,0", "0.012")]
+        [InlineData("50000,56", "108", "0.012")]
+        [InlineData("Hello world!", "108", "0.012")]
+        [InlineData("50000", "Hello world!", "0.012")]
+        [InlineData("50000", "108", "Hello world!")]
+        public void ParseTest_OnlyNumbers(string loanAmount, string duration, string nominalRate)
+        {
+            string[] args = new string[] { loanAmount, duration, nominalRate };
+            var exceptionType = typeof(ArgumentException);
+            var expectedMessage = "The calculator accept only numbers";
+
+            var result = Record.Exception(() => arguments.Parse(args));
+
+            Assert.NotNull(result);
+            Assert.IsType(exceptionType, result);
+            Assert.Equal(expectedMessage, result.Message);
         }
     }
 }
